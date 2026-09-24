4b501e212c5cd0f4f1fac253fdde321e0f49efb1
canilarautosdorei-ux <canilarautosdorei@gmail.com>
2026-09-24 16:35:28 -0300

    Point Jukebox Admin Android to kiosk admin

--- app/src/main/java/site/r777b/jukebox/MainActivity.java
+++ app/src/main/java/site/r777b/jukebox/MainActivity.java
@@ -44,8 +44,8 @@ import java.util.Locale;
 
 public class MainActivity extends Activity {
     private static final String TAG = "JukeboxWebView";
-    private static final String START_URL = "https://teste.r777b.site/jukebox/admin";
-    private static final String START_HOST = "teste.r777b.site";
+    private static final String START_URL = "https://kiosk.r777b.site/admin";
+    private static final String START_HOST = "kiosk.r777b.site";
     private static final long REFRESH_AFTER_MS = 24L * 60L * 60L * 1000L;
     private static final long RETRY_DELAY_MS = 4000L;
     private static final long PAGE_LOAD_TIMEOUT_MS = 40000L;
